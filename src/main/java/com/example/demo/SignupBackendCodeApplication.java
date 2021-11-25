5d72c051789e88fcc217e713121ae028dbd76dd8
ShenronAsh <ashishfan2@gmail.com>
2021-11-26 00:31:14 +0530

    Second

---
+++ src/main/java/com/example/demo/SignupBackendCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupBackendCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignupBackendCodeApplication.class, args);
+	}
+
+}
